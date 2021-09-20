9a362ac779e3d6124bf963275d6fec83d6a04f8e
Siddharth Pugalia <sidpug@gmail.com>
2021-09-20 12:19:44 +0530

    logo

--- app/src/main/java/com/sidpug/siddharth_pugalia/ui/history/HistoryFragment.java
+++ app/src/main/java/com/sidpug/siddharth_pugalia/ui/history/HistoryFragment.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.sidpug.siddharth_pugalia.databinding.FragmentHistoryBinding;
@@ -27,13 +24,8 @@ public class HistoryFragment extends Fragment {
         binding = FragmentHistoryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        historyViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+
         return root;
     }
 
